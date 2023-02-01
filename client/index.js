618fde2805ffa215605143ff83db5637e06449f4
Yousuf Elkhoga <yousuf.elkhoga@gmail.com>
2023-01-31 20:34:35 -0500

    added test to append root

--- client/index.js
+++ client/index.js
@@ -1,3 +1,3 @@
 let test = document.createElement(div);
-test.innerText = 'TESTING: IF YOU SEE THIS MESSAGE CLAP!'
-document.getElementById('root').appendChild()
+test.innerText = 'TESTING: IF YOU SEE THIS MESSAGE CLAP!';
+document.getElementById('root').appendChild(test);
